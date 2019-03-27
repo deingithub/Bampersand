fb3e9726a8a13fd0b503543e2ea64d52fdbcf7cd
deing <admin@15318.de>
2019-03-27 16:30:35 +0100

    Cleanup in config.cr

--- src/commands/config.cr
+++ src/commands/config.cr
@@ -10,60 +10,61 @@ module CommandsConfig
 		| config print" if args.size == 0
 
 		raise "This command can only be used in guilds" if ctx[:guild_id].nil?
+		guild = ctx[:guild_id].as(UInt64)
 		raise "Insufficient Permissions" unless Util.perms?(
 			ctx[:client],
 			ctx[:issuer].id,
-			ctx[:guild_id].as(UInt64),
+			guild,
 			Discord::Permissions::ManageGuild
 		)
 
 		return case args[0]
 		when "print"
-			"```#{State.get(ctx[:guild_id]).to_s}```"
+			"```#{State.get(guild).to_s}```"
 		when "mirror"
 			raise "Missing target channel" unless args.size == 2
 			if args[1] == "stop"
-				State.feature(ctx[:guild_id].as(UInt64), State::Features::Mirror, false)
+				State.feature(guild, State::Features::Mirror, false)
 				return "Disabled mirroring."
 			end
 			channel = Util.channel(ctx[:client], args[1])
 			raise "Invalid channel" if channel.nil?
 			raise "You can't mirror a channel into itself" if channel.id == ctx[:channel_id]
 			State.set(
-				ctx[:guild_id].as(UInt64),
+				guild,
 				{
 					mirror_in: ctx[:channel_id],
 					mirror_out: channel.id.to_u64
 				}
 			)
-			State.feature(ctx[:guild_id].as(UInt64), State::Features::Mirror, true)
+			State.feature(guild, State::Features::Mirror, true)
 			return "Mirroring to <##{channel.id}>.
 			Issue `config mirror stop` to disable."
-	when "board"
-		raise "Missing arguments" unless args.size > 1
-		if args[1] == "stop"
-			State.feature(ctx[:guild_id].as(UInt64), State::Features::Board, false)
-			return "Disabled board."
+		when "board"
+			raise "Missing arguments" unless args.size > 1
+			if args[1] == "stop"
+				State.feature(guild, State::Features::Board, false)
+				return "Disabled board."
+			end
+			raise "Invalid arguments" unless args.size == 4
+			emoji = args[1]
+			channel = Util.channel(ctx[:client], args[2])
+			raise "Invalid Channel" if channel.nil?
+			min_reacts = args[3].to_u32
+			raise "Zero is too low." if min_reacts == 0
+			State.set(
+				guild,
+				{
+					board_emoji: emoji,
+					board_channel: channel.id.to_u64,
+					board_min_reacts: min_reacts
+				}
+			)
+			State.feature(guild, State::Features::Board, true)
+			"All messages with #{min_reacts} or more #{emoji} reactions will be posted to <##{channel.id}>.
+			Issue `config board stop` to disable."
+		else
+			raise "Unknown subcommand"
 		end
-		raise "Invalid arguments" unless args.size == 4
-		emoji = args[1]
-		channel = Util.channel(ctx[:client], args[2])
-		raise "Invalid Channel" if channel.nil?
-		min_reacts = args[3].to_u32
-		raise "Zero is too low." if min_reacts == 0
-		State.set(
-			ctx[:guild_id].as(UInt64),
-			{
-				board_emoji: emoji,
-				board_channel: channel.id.to_u64,
-				board_min_reacts: min_reacts
-			}
-		)
-		State.feature(ctx[:guild_id].as(UInt64), State::Features::Board, true)
-		"All messages with #{min_reacts} or more #{emoji} reactions will be posted to <##{channel.id}>.
-		Issue `config board stop` to disable."
-	else
-		raise "Unknown subcommand"
-	end
-}
+	}
 end
