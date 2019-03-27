d7247f5ac4f7bbe1a17b42ebf0e6f571428ec824
deing <admin@15318.de>
2019-03-27 17:44:20 +0100

    Why am I stupid

--- src/State.cr
+++ src/State.cr
@@ -47,7 +47,7 @@ module State
 		@@state[guild_id]
 	end
 	def set(guild_id, update)
-		new_state = @@state[guild_id].merge(update)
+		new_state = get(guild_id).merge(update)
 		@@state[guild_id] = new_state
 		Bampersand::DATABASE.exec "insert into state (guild_id, features, mirror_in, mirror_out, board_emoji, board_channel, board_min_reacts) values (?,?,?,?,?,?,?)",
 		guild_id.to_i64,
