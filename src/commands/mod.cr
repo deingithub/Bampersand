96e49e747752cce3d65c001168056979b4aad117
deing <admin@15318.de>
2019-04-18 19:35:13 +0200

    Added warn command

--- src/commands/mod.cr
+++ src/commands/mod.cr
@@ -100,3 +100,56 @@ Commands.register_command("unmute") do |args, ctx|
   end
   {title: "", text: output}
 end
+
+WARN_HELP = {
+  text: "| warn add <@user> <optional reason>
+  | warn list <@user>
+  | warn remove <@user>
+  | warn expunge <@user>",
+  title: "**BAMPERSAND WARNINGS**",
+}
+
+Commands.register_command("warn") do |args, ctx|
+  Util.assert_guild(ctx)
+  Util.assert_perms(ctx, ManageMessages)
+  if args.size == 0
+    next WARN_HELP
+  end
+  Arguments.assert_count(args, 2)
+  command = args.shift.downcase
+  target_user = Arguments.to_user(args.shift)
+  case command
+  when "add"
+    reason = if args.size > 0
+               args.join(" ")
+             else
+               ""
+             end
+    Bampersand::DATABASE.exec "insert into warnings (guild_id, user_id, mod_id, text) values (?,?,?,?)", ctx[:guild_id].not_nil!.to_i64, target_user.id.to_u64.to_i64, ctx[:issuer].id.to_u64.to_i64, reason
+    {
+      title: "Warning added for #{target_user.username}##{target_user.discriminator}",
+      text:  "Responsible Moderator: <@#{ctx[:issuer].id}>\n#{reason}",
+    }
+  when "list"
+    output = ""
+    count = 0
+    Bampersand::DATABASE.query "select mod_id, text, timestamp from warnings where guild_id == ? and user_id == ?", ctx[:guild_id].not_nil!.to_i64, target_user.id.to_u64.to_i64 do |rs|
+      rs.each do
+        mod_id = rs.read(Int64)
+        text = rs.read(String)
+        timestamp = rs.read(String)
+        output += "**##{count + 1}** #{timestamp} UTC (<@#{mod_id}>)\n#{text}\n"
+        count += 1
+      end
+    end
+    {title: "**#{target_user.username}##{target_user.discriminator}: #{count} warning/s**".upcase, text: output}
+  when "remove"
+    Bampersand::DATABASE.exec "delete from warnings where guild_id == ? and user_id == ? limit 1", ctx[:guild_id].not_nil!.to_i64, target_user.id.to_u64.to_i64
+    true
+  when "expunge"
+    Bampersand::DATABASE.exec "delete from warnings where guild_id == ? and user_id == ?", ctx[:guild_id].not_nil!.to_i64, target_user.id.to_u64.to_i64
+    true
+  else
+    raise "Unknown subcommand"
+  end
+end
